c1d299331db45f50bbf46aae3eaca77dbc329112
Azat Khamzin <AzRKhamzin@gmail.com>
2023-08-15 10:04:43 +0300

    init logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const (
+	localEnv = "local"
+	prodEnv  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
+	logger := setupLogger(cfg.Env)
+
+	logger.Info("starting url-shortener")
+	logger.Debug("debug messages are enabled")
 	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case localEnv:
+		logger = slog.New(
+			slog.NewTextHandler(
+				os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case prodEnv:
+		logger = slog.New(slog.NewJSONHandler(
+			os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return logger
 }
